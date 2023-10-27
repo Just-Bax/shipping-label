c4cfe27d739999b4c6589860c2e4e3c5ad580d79
Just-Bax <isfandiyorbaxtiyorov132@gmail.com>
2023-10-27 12:40:08 +0500

    add repository name

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 
 export default defineConfig({
+  base: '/product-label',
   plugins: [vue(), vueJsx()],
   css: {
     postcss: {
